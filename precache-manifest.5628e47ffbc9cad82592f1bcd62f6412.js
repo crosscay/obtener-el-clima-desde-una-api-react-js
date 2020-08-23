145c4ed9686646710e309e298f8cf815233c46f6
crosscay <caicedocarlos0306@gmail.com>
2020-08-23 11:01:55 -0500

    Updates

---
+++ precache-manifest.5628e47ffbc9cad82592f1bcd62f6412.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c743328a790d3636ee95765ca88f946f",
+    "url": "/obtener_el_clima_desde_una_api_con_react/index.html"
+  },
+  {
+    "revision": "291825e02749d21ff0f0",
+    "url": "/obtener_el_clima_desde_una_api_con_react/static/css/main.3e82f7ce.chunk.css"
+  },
+  {
+    "revision": "01b9df6264e064fe279b",
+    "url": "/obtener_el_clima_desde_una_api_con_react/static/js/2.e4072910.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/obtener_el_clima_desde_una_api_con_react/static/js/2.e4072910.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "291825e02749d21ff0f0",
+    "url": "/obtener_el_clima_desde_una_api_con_react/static/js/main.ec80903b.chunk.js"
+  },
+  {
+    "revision": "6824f6e05ae1d8c2d0c1",
+    "url": "/obtener_el_clima_desde_una_api_con_react/static/js/runtime-main.aa1e5c84.js"
+  }
+]);
