1567560f65f2bfbd177c0271a1001a8391eef1c8
crosscay <caicedocarlos0306@gmail.com>
2020-08-22 20:46:17 -0500

    funcion para consultar la api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import Header from './components/Header';
 import Formulario from './components/Formulario';
 
 function App() {
+
+  // state del formulario
+  const [ busqueda, guardarBusqueda ] = useState({
+    ciudad: '',
+    pais: ''
+  });
+
+  const [consultar, guardarConsultar] = useState(false); 
+
+  const { ciudad, pais } = busqueda;
+
+  useEffect(() => {
+    // console.log(ciudad);
+      const consultarApi = async () => {
+        if(consultar) {
+          const appId = '40c594913d142621459222ad829a10a4';
+          const url = `http://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`;
+
+          const respuesta = await fetch(url);
+          const resultado = await respuesta.json();
+
+          console.log(resultado);
+        }
+    } 
+    consultarApi();
+  }, [consultar]);
+
   return (
     <Fragment>
       <Header
@@ -13,7 +40,9 @@ function App() {
           <div className="row">
             <div className="col m6 s12">
               <Formulario 
-              
+                busqueda={busqueda}
+                guardarBusqueda={guardarBusqueda}
+                guardarConsultar={guardarConsultar}
               />
             </div>
             <div className="col m6 s12">
--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,12 +1,8 @@
 import React, { useState } from 'react';
 
-const Formulario = () => {
+const Formulario = ({busqueda, guardarBusqueda, guardarConsultar}) => {
 
-    // state del formulario
-    const [ busqueda, guardarBusqueda ] = useState({
-        ciudad: '',
-        pais: ''
-    });
+    const [ error, guardarError ] = useState(false);
 
     // extraer ciudad y pais
     const { ciudad, pais } = busqueda;
@@ -20,8 +16,27 @@ const Formulario = () => {
         });
     }
 
+    // Cuando el usuario da submit al form
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        // validar
+        if(ciudad.trim() === '' || pais.trim() === '') {
+            guardarError(true);
+            return;
+        }
+
+        guardarError(false);
+
+        // pasarlo al componente principal
+        guardarConsultar(true);
+    }
+
     return ( 
-        <form>
+        <form
+            onSubmit={handleSubmit}
+        >
+            { error ? <p className="red darken-4 error">Todos los campos son obligatorios</p> : null}
             <div className="input-field cold s12">
                 <input
                     type="text"
@@ -49,6 +64,14 @@ const Formulario = () => {
                     <option value="PE">Perú</option>
                 </select>
                 <label htmlFor="pais"> País: </label>
+            </div>
+
+            <div className="input-field col s12">
+                <input
+                    type="submit"
+                    value="Buscar Clima"
+                    className="waves-effect waves-light btn-large btn-block yellow accent-4"
+                />
             </div>
         </form>
     );
